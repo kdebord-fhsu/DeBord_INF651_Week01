8c585a188ca7e556fc166a773d5eb6b19d37ac1c
kjdebord-fhsu <kjdebord@mail.fhsu.edu>
2022-09-17 15:55:28 -0800

    first commit

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+// Question 1
+var myNumber;
+
+// Question 2
+myNumber =5;
+
+// Question 3
+var myOtherNumber =10;
+
+// Question 4
+var myTotal =myNumber*myOtherNumber;
+
+// Question 5
+var myName ="";
+var myCombo ="";
+
+// Question 6
+var myOtherCombo = myNumber+myOtherNumber;
+
+// Question 7
+var myRemainder =myNumber/5;
+
+// Question 8
+var i=2;
+
+// Question 9
+var int=4;
+
+// Question 10
+var myBoolean =false;
